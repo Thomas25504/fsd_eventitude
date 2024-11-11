fd2130f3eaee6461c0b5a2507fda65db2ae8e34f
Tom Davidson <tldavidson2004@gmail.com>
2024-11-11 18:01:05 +0000

    Implement question controller with create, delete, upvote, and downvote functions

---
+++ backend/app/controllers/question.sever.controllers.js
@@ -0,0 +1,22 @@
+const createQuestion = (req, res) => {
+
+};
+
+const deleteQuestion = (req, res) => {
+
+};
+
+const upvoteQuestion = (req, res) => {
+
+}
+
+const downvoteQuestion = (req, res) => {
+
+};
+
+module.exports = {
+    createQuestion: createQuestion,
+    deleteQuestion: deleteQuestion,
+    upvoteQuestion: upvoteQuestion,
+    downvoteQuestion: downvoteQuestion
+}
