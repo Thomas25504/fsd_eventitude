3c329bd7b7a3aa31365d81410a9f7dc1607a80e1
Tom Davidson <tldavidson2004@gmail.com>
2024-11-11 18:09:15 +0000

    Implement question routes for create, delete, upvote, and downvote functionalities

--- backend/app/routes/question.server.routes.js
+++ backend/app/routes/question.server.routes.js
@@ -1,8 +1,8 @@
 const question = require('../controllers/question.server.controller');
 
 module.exports = function(app) {
-    app.route('/event/{event_id}/question').post();
-    app.route('/question/{question_id}').delete();
-    app.route('/question/{question_id}/vote').post();
-    app.route('/question/{question_id}/vote').delete();
+    app.route('/event/{event_id}/question').post(question.createQuestion);
+    app.route('/question/{question_id}').delete(question.deleteQuestion);
+    app.route('/question/{question_id}/vote').post(question.upvoteQuestion);
+    app.route('/question/{question_id}/vote').delete(question.downvoteQuestion);
 }
