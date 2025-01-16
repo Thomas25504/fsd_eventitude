32b80521c38c9229782e97ef9ac29634c9d9238f
Tom Davidson <tldavidson2004@gmail.com>
2025-01-16 13:07:21 +0000

    Enhance event handling by implementing event retrieval, adding error handling, and enforcing authentication on event routes

--- backend/app/controllers/event.server.controllers.js
+++ backend/app/controllers/event.server.controllers.js
@@ -1,4 +1,5 @@
 const events = require('../models/event.server.models');
+const users = require('../models/user.server.models');
 const validator = require('../lib/validator');
 
 const create = (req, res) => {
@@ -21,11 +22,19 @@ const create = (req, res) => {
 };
 
 const getEvent = (req, res) => {
-    return res.sendStatus(500);
+    let id = events.getIdParams(req);
+    events.getEventByID(id, (err, row) => {
+        if(err){
+            return res.status(400).send({error_message: err});
+        }
+        else{
+            return res.status(200).send(row);
+        }
+    });
 };
 
 const updateEvent = (req, res) => {
-    return res.sendStatus(401);
+    return res.sendStatus(500);
 };
 
 const deleteEvent = (req, res) => {
--- backend/app/models/event.server.models.js
+++ backend/app/models/event.server.models.js
@@ -1,4 +1,5 @@
 const db = require('../../config/database');
+const { getEvent } = require('../controllers/event.server.controllers');
 
 const insert = function(event, done){
     let values = [event.name, event.description ,event.location, event.start, event.close_registration, event.max_attendees, event.creator_id];
@@ -11,6 +12,21 @@ const insert = function(event, done){
     });
 };
 
+const getIdParams = function(req){
+    return req.params.id;
+};
+
+const getEventByID = function(id, done){
+    db.get('SELECT * FROM events WHERE event_id = ?', id, function(err, row){
+        if(err){
+            return done(err);
+        }
+        return done(null, row);
+    });
+};
+
 module.exports = {
-    insert: insert
+    insert: insert,
+    getIdParams: getIdParams,
+    getEventByID: getEventByID
 }
